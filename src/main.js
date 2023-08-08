24f3e0bec5d6923fd8f8bf72678474c487137a07
fabio thomaz <fabio_thomaz@live.com>
2023-08-07 22:50:45 -0400

    convertido para pwa

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import firebase, { firebaseApp } from '../src/plugins/firebase'
+import './registerServiceWorker'
 
 
 Vue.use(firebase)
@@ -18,15 +19,3 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
-
-//service worker
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-    .then(registration => {
-      console.log('Service Worker registrado com sucesso:', registration);
-    })
-    .catch(error => {
-      console.error('Falha ao registrar o Service Worker:', error);
-    });
-}
-
